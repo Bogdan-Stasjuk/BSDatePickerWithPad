9bfff9c8442ae7f140e0e69e229e1e047b2f3cfb
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2014-06-06 16:43:46 +0300

    Initial Commit

---
+++ UIDatePickerTextFieldUsage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIDatePickerTextFieldUsage
+//
+//  Created by Bogdan Stasjuk on 6/6/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
